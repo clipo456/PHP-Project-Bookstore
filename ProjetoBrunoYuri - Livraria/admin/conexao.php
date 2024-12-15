5ab3c3945b4318363c070de7cf34fd4a66201f2c
Yuri Maximo <81641618+clipo456@users.noreply.github.com>
2024-12-15 14:51:51 -0300

    Delete ProjetoBrunoYuri - Livraria directory

--- ProjetoBrunoYuri - Livraria/admin/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    //Define as informações de acesso 
-    define('DB_SERVIDOR','localhost:3306');
-    define('DB_USUARIO','root');
-    define('DB_SENHA','12345678');
-    define('DB_BANCO','livraria');
-
-    $dbc = @mysqli_connect(DB_SERVIDOR,DB_USUARIO,DB_SENHA,DB_BANCO) or die ("Não foi possível conectar ao MYSQL: " . mysqli_connect_error());
-?>
